a79b1e1b290228decd7416d6f692cbe38451f2ee
Mert <bilgic.mert44@hotmail.com>
2021-03-11 10:44:57 +0300

    Created error pages

--- bcommerce/settings.py
+++ bcommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'f74@958n#(wat_y%fqjl3s)=uz6(t075i2a833n4!u&+m58t@#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
